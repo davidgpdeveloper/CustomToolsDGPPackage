17a3ef3b604e0bf9d3cc2f5217498711bb5808c2
David Galán <dgalan@opentrends.net>
2021-01-28 21:10:27 +0100

    Added AddressTools file.

---
+++ Sources/CustomToolsDGPPackage/Files/AddressTools.swift
@@ -0,0 +1,105 @@
+//
+//  AddressTools.swift
+//  CustomToolsDGP
+//
+//  Created by David SG on 03/06/2020.
+//  Copyright © 2020 David Galán. All rights reserved.
+//
+
+#if canImport(UIKit)
+import UIKit
+#endif
+import MapKit
+
+
+extension String {
+    
+    public func openAddress(typeAddressResult: AddressTools.TypeAddressResult, completion: @escaping (Bool) -> ()) {
+
+        AddressTools().openAddressActions(address: self, typeAddressResult: typeAddressResult) { (success) in
+            
+            if success {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+    
+}
+
+
+
+public class AddressTools {
+
+    public enum TypeAddressResult {
+        case address
+        case location
+    }
+    
+    public init() {
+        
+    }
+    
+    
+    func openAddressActions(address: String, typeAddressResult: TypeAddressResult, completion: @escaping (Bool) -> ()) {
+                
+        switch typeAddressResult {
+
+        case .address:
+            
+            guard let address = URL(string: "http://maps.apple.com/?address=\(address)") else {
+                completion(false)
+                return
+            }
+            #if canImport(UIKit)
+            UIApplication.shared.open(address)
+            #endif
+            completion(true)
+            
+            break
+            
+        case .location:
+            
+            self.coordinates(forAddress: address, completion: { (location) in
+                
+                if location != nil {
+                    let query = "?ll=\(location!.latitude),\(location!.longitude)"
+                    let path = "http://maps.apple.com/" + query
+                    if let url = URL(string: path) {
+                        #if canImport(UIKit)
+                        UIApplication.shared.open(url)
+                        #endif
+                        completion(true)
+                        
+                    } else {
+                        // Could not construct url. Handle error.
+                        print("nop2")
+                        completion(false)
+                    }
+                } else {
+                    print("nop1")
+                    completion(false)
+                }
+                
+            })
+            
+            break
+        }
+    }
+    
+    
+    func coordinates(forAddress address: String, completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) {
+            (placemarks, error) in
+            guard error == nil else {
+                print("Geocoding error: \(error!)")
+                completion(nil)
+                return
+            }
+            completion(placemarks?.first?.location?.coordinate)
+        }
+    }
+    
+}
--- Tests/CustomToolsDGPPackageTests/CustomToolsDGPPackageTests.swift
+++ Tests/CustomToolsDGPPackageTests/CustomToolsDGPPackageTests.swift
@@ -6,7 +6,7 @@ final class CustomToolsDGPPackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CustomToolsDGPPackage().text, "Hello, World!")
+//        XCTAssertEqual(CustomToolsDGPPackage().text, "Hello, World!")
     }
 
     static var allTests = [
